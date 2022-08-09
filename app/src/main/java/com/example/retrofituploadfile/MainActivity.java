3369bc2652ba11284dc77f8d0d41529e3146b1c3
cuongvv205 <101417205+cuongvv205@users.noreply.github.com>
2022-08-09 09:55:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/retrofituploadfile/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.retrofituploadfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//    http://accountservermanagement.herokuapp.com/api/accounts
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
